4a574ae11938e97609e31b0fcc33374f8b96cb6a
Bo Peng <ben.bog@gmail.com>
2020-03-27 12:51:46 -0500

    Move common parameters to the top

--- src/penn_chime/settings.py
+++ src/penn_chime/settings.py
@@ -12,16 +12,24 @@ Houston = 7100000
 # which I expect matches well the average behavior.  For Harris Health, I will assume 23% reduction in social distancing and for
 # Methodist I will assume 63% because I think these two entities serve patient populations that have very different needs and behaviors.
 
-# Attached is screenshot of what I think the baseline model should be for BSLMC with 43% social distancing. 
+# Attached is screenshot of what I think the baseline model should be for BSLMC with 43% social distancing.
 # For Harris Health I think all is the same except social distancing I will assume to be 23%. For Methodist
-# the same but distancing is 63%. Also, perhaps the number of people in the metropolitan Houston area at 
-# 4 million is too low? 7.1 M is for Houston+Galveston which is too large. Also I think we should set the 
-# baseline model for 30 days not 60. Other parameters should be as indicated. We will have to change 
+# the same but distancing is 63%. Also, perhaps the number of people in the metropolitan Houston area at
+# 4 million is too low? 7.1 M is for Houston+Galveston which is too large. Also I think we should set the
+# baseline model for 30 days not 60. Other parameters should be as indicated. We will have to change
 # number of Hospitalized COVID-19 patients each time.
 
 common_doubling_time = 7
 common_n_days = 30
-
+common_hospital_length_of_stay = 11
+common_hospitalization_rate = 0.044    # 0.044 means 4.4%
+common_icu_rate = 0.0075
+common_icu_legnth_of_stay = 10
+common_ventilated_rate = 0.005
+common_ventilated_length_of_stay = 10
+#
+# NOTE: RateLos is rate + length of stay.
+#
 if os.environ['SITE'] == 'BCM':
     DEFAULTS = Constants(
         # EDIT YOUR DEFAULTS HERE
@@ -34,9 +42,9 @@ if os.environ['SITE'] == 'BCM':
         n_days=common_n_days,
         market_share=0.15,
         relative_contact_rate=0.63,
-        hospitalized=RateLos(0.025, 7),
-        icu=RateLos(0.0075, 9),
-        ventilated=RateLos(0.005, 10),
+        hospitalized=RateLos(common_hospitalization_rate, common_hospital_length_of_stay),
+        icu=RateLos(common_icu_rate, common_icu_legnth_of_stay),
+        ventilated=RateLos(common_ventilated_rate, common_ventilated_length_of_stay),
     )
 elif os.environ['SITE'] == 'METHODIST':
     DEFAULTS = Constants(
@@ -50,9 +58,9 @@ elif os.environ['SITE'] == 'METHODIST':
         n_days=common_n_days,
         market_share=0.15,
         relative_contact_rate=0.63,
-        hospitalized=RateLos(0.025, 7),
-        icu=RateLos(0.0075, 9),
-        ventilated=RateLos(0.005, 10),
+        hospitalized=RateLos(common_hospitalization_rate, common_hospital_length_of_stay),
+        icu=RateLos(common_icu_rate, common_icu_legnth_of_stay),
+        ventilated=RateLos(common_ventilated_rate, common_ventilated_length_of_stay),
     )
 elif os.environ['SITE'] == 'BSLMC':
     DEFAULTS = Constants(
@@ -66,9 +74,9 @@ elif os.environ['SITE'] == 'BSLMC':
         n_days=common_n_days,
         market_share=0.15,
         relative_contact_rate=0.43,
-        hospitalized=RateLos(0.025, 7),
-        icu=RateLos(0.0075, 9),
-        ventilated=RateLos(0.005, 10),
+        hospitalized=RateLos(common_hospitalization_rate, common_hospital_length_of_stay),
+        icu=RateLos(common_icu_rate, common_icu_legnth_of_stay),
+        ventilated=RateLos(common_ventilated_rate, common_ventilated_length_of_stay),
     )
 elif os.environ['SITE'] == 'HarrisHealth':
     DEFAULTS = Constants(
@@ -82,9 +90,9 @@ elif os.environ['SITE'] == 'HarrisHealth':
         n_days=common_n_days,
         market_share=0.15,
         relative_contact_rate=0.23,
-        hospitalized=RateLos(0.025, 7),
-        icu=RateLos(0.0075, 9),
-        ventilated=RateLos(0.005, 10),
+        hospitalized=RateLos(common_hospitalization_rate, common_hospital_length_of_stay),
+        icu=RateLos(common_icu_rate, common_icu_legnth_of_stay),
+        ventilated=RateLos(common_ventilated_rate, common_ventilated_length_of_stay),
     )
 else:
     raise ValueError(f'Invalid SITE {os.environ["SITE"]}')
